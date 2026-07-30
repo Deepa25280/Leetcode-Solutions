48e56413f25a4d6038d0b2cb808413eae6fcc254
Deepa Yadav <deepay32947@gmail.com>
2026-07-30 23:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (91.23%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean vis[][] = new boolean[image.length][image[0].length];
+        helper(image, sr, sc, color, vis, image[sr][sc]);
+        return image;
+    }
+
+    public void helper(int[][] image, int sr, int sc, int color, boolean[][] vis, int orgColor) {
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || vis[sr][sc]
+                || image[sr][sc] != orgColor) {
+            return;
+        }
+        vis[sr][sc] = true;
+        image[sr][sc] = color;
+
+        helper(image, sr, sc + 1, color, vis, orgColor);
+        helper(image, sr, sc - 1, color, vis, orgColor);
+
+        helper(image, sr + 1, sc, color, vis, orgColor);
+        helper(image, sr - 1, sc, color, vis, orgColor);
+
+    }
+}
